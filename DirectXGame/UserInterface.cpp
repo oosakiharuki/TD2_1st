2494c8c3bd5d390b32ec57fd3dbcc3f8790a99df
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-10-21 11:38:25 +0900

    HPの追加(プレイヤーの当たり判定に接続予定)

--- DirectXGame/UserInterface.cpp
+++ DirectXGame/UserInterface.cpp
@@ -3,20 +3,40 @@
 UserInterface::~UserInterface() {
 	delete sprite2Dgauge ;
 	delete sprite2Dcharge;
+	for (Sprite* heart : sprite2DHP_) {
+		if (heart) {
+			delete heart;
+			heart = nullptr;
+		}
+	}
+	sprite2DHP_.clear();
 }
 
 void UserInterface::Initialize() {
+
+	// レティクル用テクスチャ取得
+	uint32_t GaugeReticle = TextureManager::Load("gauge.png");
+	uint32_t ChargeReticle = TextureManager::Load("charge.png");
+	uint32_t HPReticle = TextureManager::Load("HP.png");
 
 	gaugepos_ = { 1000.0f,30.0f };
 	gaugecolor_ = { 1.0f,1.0f,1.0f,1.0f };
 	chargepos_ = { 1000.0f,30.0f };
 	chargecolor_ = { 1.0f,1.0f,1.0f,1.0f };
-
-
 	chargesize_ = { 0.0f,60.0f };
-	// レティクル用テクスチャ取得
-	uint32_t GaugeReticle = TextureManager::Load("gauge.png");
-	uint32_t ChargeReticle = TextureManager::Load("charge.png");
+	HPpos_ = {40.0f, 17.0f};
+	HPcolor_ = {1.0f, 1.0f, 1.0f, 1.0f};
+
+	// 3つのハートを初期化
+	for (int i = 0; i < 3; ++i) {
+		Sprite* newsprite2DHP = new Sprite();
+		newsprite2DHP->Initialize();
+		Vector2 position = {40.0f + i * 47.0f, 17.0f};
+		newsprite2DHP = Sprite::Create(HPReticle, HPpos_, HPcolor_);
+		newsprite2DHP->SetPosition(position);
+		sprite2DHP_.push_back(newsprite2DHP);
+	}
+
 	// スプライト生成
 	sprite2Dgauge = Sprite::Create(GaugeReticle, gaugepos_, gaugecolor_);
 	sprite2Dcharge = Sprite::Create(ChargeReticle, chargepos_, chargecolor_);
@@ -29,8 +49,25 @@ void UserInterface::Initialize() {
 void UserInterface::Update() {
 
 	if (input_->PushKey(DIK_SPACE) && chargesize_.x == 200) {
-		attack_ = true;
+		attack_ = true;	
 	}
+
+	if (input_->PushKey(DIK_S) && timeSinceLastRemove >= removeInterval) {
+		HPfige_ = IsButtonPressed();
+	}
+
+	if (!HPfige_) {
+		timeSinceLastRemove += 1;
+	}
+
+	 // ボタンが押されたらハートを削除
+	if (HPfige_) {
+		RemoveHeart();
+		timeSinceLastRemove = 0;
+		HPfige_ = false;
+	}
+
+
 
 	if (attack_) {
 		attack_ = true;
@@ -55,17 +92,21 @@ void UserInterface::Update() {
 	sprite2Dgauge->SetPosition(gaugepos_);
 	sprite2Dcharge->SetPosition(chargepos_);
 	sprite2Dcharge->SetSize(chargesize_);
-
-
-	//ImGui::Begin("UI");
-	////ImGui::Checkbox("Check", &fige_);
-	//ImGui::DragFloat2("gauge", &gaugepos_.x);
-	//ImGui::DragFloat2("charge", &chargepos_.x);
-	//ImGui::DragFloat2("size", &chargesize_.x);
-	//ImGui::End();
 }
 
 void UserInterface::Draw2D() {
 	sprite2Dcharge->Draw();
 	sprite2Dgauge->Draw();
+	for (Sprite* HP : sprite2DHP_){
+		HP->Draw();
+	}
+}
+
+void UserInterface::RemoveHeart() {
+	if (!sprite2DHP_.empty()) {
+		Sprite* lastHeart = sprite2DHP_.back();
+
+		delete lastHeart;
+		sprite2DHP_.pop_back();
+	}
 }
--- DirectXGame/UserInterface.h
+++ DirectXGame/UserInterface.h
@@ -4,9 +4,9 @@
 #include <base\TextureManager.h>
 #include<3d/Model.h>
 #include<3d/WorldTransform.h>
-#include<imgui.h>
 #include<base/WinApp.h>
 #include<input/Input.h>
+#include<list>
 
 class UserInterface
 {
@@ -25,25 +25,36 @@ public:
 		return attack_;
 	}
 
+	bool IsButtonPressed() {
+		return true;
+	}
+
+	void RemoveHeart();
+
 private:
 
 	// 2Dスプライト
-	Sprite* sprite2Dgauge = nullptr;          // ゲージ
-	Sprite* sprite2Dcharge = nullptr;          // チャージ
+	Sprite* sprite2Dgauge  = nullptr;           // ゲージ
+	Sprite* sprite2Dcharge = nullptr;           // チャージ
+	std::list<Sprite*> sprite2DHP_;             // ハート
 	// 座標
 	Vector2 gaugepos_;
 	Vector2 chargepos_;
+	Vector2 HPpos_;
 	// カラー
 	Vector4 gaugecolor_;
 	Vector4 chargecolor_;
+	Vector4 HPcolor_;
 	// サイズ
 	Vector2 chargesize_;
 	// フラグ
 	bool Chargeswitch_ = false;
 	bool attack_ = false;
+	bool HPfige_ = false;
+	uint32_t timeSinceLastRemove = 0; 
+	uint32_t removeInterval = 30;
 
 	// キーボード入力
 	Input* input_ = nullptr;
-
 };
 
